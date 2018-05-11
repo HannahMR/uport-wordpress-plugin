4a296a07e03af9360a1dbe7433c956d1f891f459
raininja <daniel.mclellan@gmail.com>
2018-05-10 22:26:39 -0400

    refactored for js workflow

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import TopicFactoryDesktop from './TopicFactoryDesktop.js'
---
+++ webpack.config.babel.js
@@ -0,0 +1,99 @@
+// import fs                 from fs;
+import path               from 'path';
+import CopyWebpackPlugin  from 'copy-webpack-plugin';
+import BrowserSyncPlugin  from 'browser-sync-webpack-plugin';
+import ZipPlugin          from 'zip-webpack-plugin';
+import webpack            from 'webpack';
+
+const isProd = process.env.NODE_ENV === 'development';
+
+//////////////////////// FILEPATH ///////////////////////
+/////////////////////////////////////////////////////////
+
+const buildFolder  = 'dist',
+      sourceFolder = 'src',
+      PATHS = {
+        build        : path.resolve(__dirname, buildFolder),
+        src          : path.resolve(__dirname, sourceFolder),
+        node_modules : path.resolve(__dirname, 'node_modules'),
+        index        : path.resolve(__dirname, `${sourceFolder}/js/index.js`),
+        zipped       : path.join(__dirname, buildFolder),
+
+      };
+
+//////////////////////// PLUGINS ////////////////////////
+/////////////////////////////////////////////////////////
+
+const CopyWebpackPluginOptions = ([
+  {from: path.resolve(__dirname, 'src/php') + '/**',
+     to: buildFolder},
+
+  {from: path.resolve(__dirname, 'src/wp-uport.php'),
+     to: buildFolder},
+   ],
+   {copyUnmodified: true}
+);
+
+const ZipPluginOptions = {
+  filename: 'wp-uport.zip',
+  pathPrefix: 'relative/path',
+  zipOptions: {forceZip64Format: false}
+};
+
+const devServerOptions = {
+  contentBase: PATHS.build,
+  compress: true,
+  port: 8080,
+  stats: 'errors-only',
+  open: true,
+  hot: false,
+  openPage: ''
+};
+
+const pluginsDev = [
+  new webpack.HotModuleReplacementPlugin(),
+  new CopyWebpackPlugin([CopyWebpackPluginOptions]),
+  new ZipPlugin(ZipPluginOptions)
+];
+
+const pluginsProd = [
+  new CopyWebpackPlugin([CopyWebpackPluginOptions]),
+  new ZipPlugin(ZipPluginOptions)
+];
+
+var pluginsList = isProd ? pluginsProd : pluginsDev;
+
+//////////////////////// LOADERS ////////////////////////
+/////////////////////////////////////////////////////////
+
+const javascript = {
+  test: /\.(js|jsx)$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  query: {
+    presets: ['es2015']
+  }
+};
+
+
+//////////////////////// WEBPACK ////////////////////////
+/////////////////////////////////////////////////////////
+
+const webpackConfig = {
+  mode: 'production',
+  entry: PATHS.index,
+  output:  {
+    path: PATHS.zipped,
+    filename: 'wp_uport.js'
+  },
+  module: {
+    rules: [javascript]
+  },
+  resolve: {
+    modules: [PATHS.src, 'node_modules'],
+  },
+  devServer: devServerOptions,
+  plugins: pluginsList
+};
+
+export default webpackConfig;
