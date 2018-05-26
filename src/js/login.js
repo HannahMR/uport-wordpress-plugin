e3dddfc76cb32c24225f611e821409f1b2966fe4
raininja <daniel.mclellan@gmail.com>
2018-05-26 01:33:16 -0400

    further testing

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,10 @@
 //@flow
-import { Connect, SimpleSigner } from 'uport-connect'
+import {Connect, ConnectCore, QRUtil, SimpleSigner, Credentials, MNID} from 'uport-connect'
 import Web3 from 'web3'
 
 // const Connect = window.uportconnect.Connect
-const appName = 'uport-wp-plugin'
-const uport = new Connect(appName, {
+// d'application registry infos
+const uport = new Connect('uport-wp-plugin', {
   clientId: '2onpaDYj2R4RaUJGJcWe3AAWoQn5e4kwQzo',
   network: 'rinkeby',
   signer: SimpleSigner('20c2eb928659799c4cf6e25177d0a68f26819774d30d0127d2aaa0beae84b258'),
@@ -40,49 +40,9 @@ const abi = [{
 const StatusContract = web3.eth.contract(abi);
 const statusInstance = StatusContract.at('0x70A804cCE17149deB6030039798701a38667ca3B')
 
-// uPort connect
-const uportConnect = function() {
-  console.log("clicked")
-  web3.eth.getCoinbase((error, address) => {
-    if (error) { throw error }
-    globalState.ethAddress = address
-    console.log("address")
+      // State and render functions
 
-    // This one is for display purposes - MNID encoding includes network
-    globalState.uportId = uport.MNID.encode({network: '0x4', address: address})
-    statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
-      globalState.currentStatus = st
-      web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
-        globalState.ethBalance = web3.fromWei(bal)
-        render()
-      })
-    })
-  })
-}
-
-function ready(uportConnect) {
-  var injectButton = function(uportConnect) {
-    const loginForm = document.getElementById('loginform')
-    const buttondata = '<div class="button buttom-primary button-large" id="connectUportBtn" style="float: left" onclick="window.uportConnect">Connect uPort</div>'
-    loginForm.insertAdjacentHTML('beforeend', buttondata)
-  }
-  if (document.readyState !== 'loading' &&
-    document.getElementsByClassName('login') != null &&
-    document.getElementsByClassName('wp-core-ui') != null){
-    injectButton()
-  } else {
-      document.addEventListener('DOMContentLoaded', injectButton)
-    }
-  }
-
-// ready.onclick = uportConnect();
-window.uportConnect = uportConnect
-global.ready = ready
-
-ready()
-
-// TODO: improve this schema using HTML5 methods
-const $ = (selector) => document.querySelector(selector)
+      const $ = (selector) => document.querySelector(selector)
       let globalState = {
         uportId: "",
         ethAddress: "",
@@ -112,4 +72,83 @@ const $ = (selector) => document.querySelector(selector)
         globalState.statusInput = $('#statusInput').value;
         console.log(globalState)
       }
+
+      ////////////////////////////////////////////////
+
+
+      ////////////////////////////////////////////////
+
+
+function ready() {
+  // uPort connect
+  const uportConnect = function() {
+    console.log("clicked")
+    web3.eth.getCoinbase((error, address) => {
+      if (error) { throw error }
+      globalState.ethAddress = address
+      console.log("address")
+      // This one is for display purposes - MNID encoding includes network
+      // here we will need to include the wp cred info?
+      // test runs will use a pre-registered user
+      globalState.uportId = uport.MNID.encode({network: '0x4', address: address})
+      statusInstance.getStatus.call(globalState.ethAddress, (err, st) => {
+        globalState.currentStatus = st
+        web3.eth.getBalance(globalState.ethAddress, (err, bal) => {
+          globalState.ethBalance = web3.fromWei(bal)
+          render()
+        })
+      })
+    })
+  }
+  var injectButton = function(uportConnect) {
+    const loginForm = document.getElementById('loginform')
+    const buttondata = '<div class="button buttom-primary button-large" id="connectUportBtn" style="float: left" onclick="window.uportConnect">Connect uPort</div>'
+    loginForm.insertAdjacentHTML('beforeend', buttondata)
+  }
+  // var injectSchema = function() {
+  //   const loginel = document.getElementById('login')
+  //   const scriptdata = '\
+  //   <script>\
+  //   const $ = (selector) => document.querySelector(selector);\
+  //   let globalState = {\
+  //     uportId: "",\
+  //     ethAddress: "",\
+  //     ethBalance: "",\
+  //     currentStatus: "",\
+  //     statusInput: "",\
+  //     txHashSentEth: "",\
+  //     txHashSetStatus: "",\
+  //     sendToAddr: "",\
+  //     sendToVal: ""\
+  //   };\
+  //   const render = function () {\
+  //     $("\u0023uportId").innerHTML = globalState.uportId;\
+  //     $("\u0023currentStatus").innerHTML = globalState.currentStatus;\
+  //   }\
+  //   </script>'
+  //
+  //   // const scriptdata = '<script> // this is a comment </script>'
+  //   loginel.insertAdjacentHTML('afterend', scriptdata)
+  // }
+
+
+  if (document.readyState !== 'loading' &&
+    document.getElementsByClassName('login') != null &&
+    document.getElementsByClassName('wp-core-ui') != null){
+    injectButton()
+    // injectSchema()
+  } else {
+    // injectSchema()
+    document.addEventListener('DOMContentLoaded', injectButton)
+    // document.addEventListener('DOMContentLoaded', injectSchema)
+    }
+  }
+
+// ready.onclick = uportConnect();
+// window.uportConnect = uportConnect
+global.ready = ready
+
+ready()
+
+
 // module.exports = uportConnect;
